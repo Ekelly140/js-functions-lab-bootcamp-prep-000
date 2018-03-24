4391171e865a1ec0c5a01c785d7ef5cf7f198aa5
ERIC KELLY <eakelly220@gmail.com>
2018-03-24 19:57:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo (name){
+  return `Happy holidays, ${name}!`
+}
+
+function happyholidayTo (holiday, name){
+  return ` Happy ${holiday}, ${name}!`
+}
+
+function holidayCountDown (day, holiday){
+  return `It's ${days} until ${holiday}!`
+}
